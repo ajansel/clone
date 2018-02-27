5429199d0751c74b7365511d59f369f19c8df4ba
AJ Ansel <Andrew.ansel6@gmail.com>
2018-02-26 23:42:55 -0800

    Change session to expect email instead of username

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::SessionsController < ApplicationController
   def create
-    @user = User.find_by_credentials(user_params[:username], user_params[:password])
+    @user = User.find_by_credentials(user_params[:email], user_params[:password])
 
     if @user
       login!(@user)
@@ -24,6 +24,6 @@ class Api::SessionsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:email, :password)
   end
 end
