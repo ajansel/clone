4fec1eb1f5dc6a653defd1c5bd8ec40ba346be38
AJ Ansel <Andrew.ansel6@gmail.com>
2018-02-26 23:47:14 -0800

    Add User::find_by_credentials and User#is_password?

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,22 +6,31 @@ class User < ApplicationRecord
 
   attr_reader :password
 
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    user && user.is_password?(password) ? user : nil
+  end
+
   def ensure_session_token
     self.session_token ||= generate_session_token
-  end 
+  end
 
   def generate_session_token
     SecureRandom.urlsafe_base64
-  end 
+  end
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
-  end 
+  end
 
   def reset_session_token
     self.session_token = generate_session_token
     self.save!
     self.session_token
-  end 
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
 end
