fdfe040da9cb02052a30485e5b7074816bb69307
AJ Ansel <Andrew.ansel6@gmail.com>
2018-02-27 00:55:26 -0800

    Create Posts Controller

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,85 @@
+class Api::PostsController < ApplicationController
+  MEDIA_TYPES = ['Text', 'Photo', 'Quote', 'Link', 'Chat']
+
+  def index
+    if params[:user_id]
+      # Return all of that user's posts
+      @posts = Post.where(author_id: params[:user_id])
+    else
+      # Return all of the posts for the current user's followees
+      # TODO: Change this once followers exist
+      @posts = Post.all
+    end
+    debugger
+    render :index
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if MEDIA_TYPES.include?(@post.media_type)
+      @media = @post.media_type.classify.constantize.new(media_params)
+
+      if @media.save
+        @post.media_id = @media.id
+        @post.author_id = 1
+
+        if @post.save
+          render :show
+        else
+          @media.destory
+          render json: @post.errors.full_messages, status: 422
+        end
+      else
+        render json: @media.errors.full_messages, status: 422
+      end
+    else
+      render json: ['Invalid media type provided'], status: 422
+    end
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+    if @post
+      render :show
+    else
+      render json: ['Post does not exist'], status: 404
+    end
+  end
+
+  def update
+    @post = current_user.posts.find_by(id: params[:id])
+
+    # Post will never need to be updated, only the media itself
+    if @post
+      if @post.media.update_attributes(media_params)
+        render :show
+      else
+        render json: @post.media.errors.full_messages, status: 422
+      end
+    else
+      render json: ['This is not your post'], status: 401
+    end
+  end
+
+  def destroy
+    @post = current_user.posts.find_by(id: params[:id])
+    if @post
+      @post.destory
+      render :show
+    else
+      render json: ['This is not your post'], status: 401
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:media_type)
+  end
+
+  def media_params
+    params.require(:media).permit(:title, :body, :url, :caption,
+                                  :body, :source, :description)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: { format: :json } do 
-    resources :users, only: [:index, :create, :show]
+    resources :users, only: [:index, :create, :show] do
+      resources :posts, only: [:index]
+    end 
     resource :session, only: [:create, :destroy]
+    resources :posts, except: [:new, :edit]
   end 
 end
