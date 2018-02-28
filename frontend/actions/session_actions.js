3e8ce93294f83ae3c25754311aae875cf7f48c52
AJ Ansel <Andrew.ansel6@gmail.com>
2018-02-27 21:56:52 -0800

    Create Session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,34 @@
+import { postSession, postUser, deleteSession } from '../util/session_api_util';
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
+
+const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser 
+});
+
+const receiveSessionErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors 
+});
+
+export const signup = (formUser) => (dispatch) => (
+  postUser(formUser).then(
+    (user) => dispatch(receiveCurrentUser(user)),
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
+  )
+);
+
+export const login = (formUser) => (dispatch) => (
+  postSession(formUser).then(
+    (user) => dispatch(receiveCurrentUser(user)),
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
+  )
+);
+
+export const logout = () => dispatch => (
+  deleteSession().then(
+    (user) => dispatch(receiveCurrentUser(null)),
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
+  )
+)
