eab2b4f4396c39665b5b437f0fcba54e86a63bcb
AJ Ansel <Andrew.ansel6@gmail.com>
2018-02-27 23:58:45 -0800

    Add scroll and arrow up / down functionality to homepage

--- frontend/components/homepage/homepage.jsx
+++ frontend/components/homepage/homepage.jsx
@@ -4,9 +4,69 @@ import { Link } from 'react-router-dom';
 class Homepage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { currentPane: 0 };
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+    window.addEventListener('wheel', this.handleScroll);
+    console.log("aa");
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+    window.removeEventListener('wheel', this.handleScroll);
+    console.log("bye");
+    
+  }
+
+  handleScroll(event) {
+    window.removeEventListener('wheel', this.handleScroll);
+    console.log("start");
+    
+    let newPane;
+    if (event.deltaY < 0) {
+      newPane = this.state.currentPane + 1;
+      this.setState({
+        currentPane: newPane
+      });
+    } else if (event.deltaY > 0) {
+      newPane = this.state.currentPane - 1;
+      this.setState({
+        currentPane: newPane
+      });
+    }
+
+    // Wait 2 seconds to set another event listener to prevent continued scroll
+    const callback = () => window.addEventListener('wheel', this.handleScroll);
+    setTimeout(callback, 2000);
+  }
+
+  handleKeyDown(event) {
+    let newPane;
+    console.log(event);
+    if (event.key === "ArrowDown") {
+      newPane = this.state.currentPane + 1;
+      this.setState({
+        currentPane: newPane
+      });
+    } else if (event.key === "ArrowUp") {
+      newPane = this.state.currentPane - 1;
+      this.setState({
+        currentPane: newPane
+      });
+    }
   }
 
   render() {
-    
+    return (
+      <div>
+        {this.state.currentPane}
+      </div>
+    );
   }
 }
+
+export default Homepage;
