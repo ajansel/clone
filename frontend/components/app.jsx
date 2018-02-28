066e31c83d4a1b799906d999b7162c903cfd2b4c
AJ Ansel <Andrew.ansel6@gmail.com>
2018-02-27 23:57:47 -0800

    Add switch statement to app that renders the root (homepage)

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import HomepageContainer from './homepage/homepage_container';
+import { Route, Switch } from 'react-router-dom';
 
 const App = () => (
   <div>
     <h1>Welcome to Clone</h1>
+    <Switch>
+      <Route path="/" component={HomepageContainer} />
+    </Switch>
   </div>
 );
 
